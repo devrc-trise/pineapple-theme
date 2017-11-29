8293ab7078ebf8302b85d637ef3b0f0a41da4476
Romel Campos <rcoolah@gmail.com>
2017-11-29 19:58:47 +0800

    Reuse clients section

--- pages/page-about.php
+++ pages/page-about.php
@@ -74,45 +74,7 @@
 </div>
 
 <div id="clients-sections">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-6 col-md-4">
-        <div class="img-wrapper text-center">
-          <img src="/wp-content/uploads/2017/11/marriott.png" width="156px">
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4">
-        <div class="img-wrapper text-center">
-          <img src="/wp-content/uploads/2017/11/charlottes-w5.png" width="255px">
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4">
-        <div class="img-wrapper text-center">
-          <img src="/wp-content/uploads/2017/11/labs.png" width="187px">
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4">
-        <div class="img-wrapper text-center">
-          <img src="/wp-content/uploads/2017/11/rut.png" width="121px">
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4">
-        <div class="img-wrapper text-center">
-          <img src="/wp-content/uploads/2017/11/bnl.png" width="292px">
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4">
-        <div class="img-wrapper text-center">
-          <img src="/wp-content/uploads/2017/11/sager-wild.png" width="195px">
-        </div>
-      </div>
-      <div class="col-md-12">
-        <div class="img-wrapper text-center mb0">
-          <img src="/wp-content/uploads/2017/11/goodman.png" width="358px">
-        </div>
-      </div>
-    </div>
-  </div>
+  <?php include 'sections/clients.php' ?>
 </div>
 
 <div id="about-contact-section" class="bg-orange">
