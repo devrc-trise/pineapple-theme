3be8888eae38feed52d59c7bc953cef4cf2f93ac
Romel Campos <rcoolah@gmail.com>
2017-12-12 11:59:39 +0800

    Move out scripts from home

--- functions.php
+++ functions.php
@@ -14,6 +14,8 @@ function enqueue_custom_script() {
 function home_page_sc() {
   wp_enqueue_style('movingbubbles-style', get_stylesheet_directory_uri().'/css/movingbubbles.css');
   wp_enqueue_script('movingbubbles-script', get_stylesheet_directory_uri().'/js/movingbubbles.js');
+  wp_enqueue_script('bodymovin-script', get_stylesheet_directory_uri().'/js/bodymovin.js');
+  wp_enqueue_script('home-script', get_stylesheet_directory_uri().'/js/home.js', array('jquery'));
   get_template_part('pages/page', 'home');
 }
 add_shortcode('home_page', 'home_page_sc');
