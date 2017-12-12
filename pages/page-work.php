b68b77dec83f60a7731f8fcc4f37079d9c6464be
Romel Campos <rcoolah@gmail.com>
2017-12-12 17:34:02 +0800

    Tweak our work page

--- pages/page-work.php
+++ pages/page-work.php
@@ -10,6 +10,8 @@ $the_query = new WP_Query($args);
 while ($the_query->have_posts()) {
   $the_query->the_post();
   $background = get_post_meta($post->ID, 'background_image', true);
+  $right_title = get_post_meta($post->ID, 'right_title', true);
+  $right_content = get_post_meta($post->ID, 'right_content', true);
   ?>
 
   <div class="work-list-section" style="background-image: url(<?php echo $background ?>);">
@@ -17,10 +19,17 @@ while ($the_query->have_posts()) {
       <div class="row justify-content-center">
         <div class="col-lg-10">
           <div class="row">
-            <div class="col-xs-10 col-md-8">
+            <div class="col-xs-10 col-md-10 col-lg-8">
               <h1 class="text-uppercase"><?php echo the_title(); ?></h1>
               <div class="text-bold">
                 <?php echo the_content(); ?>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="line-white-xs line-separator pull-left"></div>
+              <div class="work-right">
+                <p class="lead-xl text-bold"><?php echo $right_title; ?></p>
+                <?php echo $right_content; ?>
               </div>
             </div>
           </div>
