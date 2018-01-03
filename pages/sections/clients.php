fe6f53e1bb568e855b8e94253d2b7cf4a899d9d9
Romel Campos <rcoolah@gmail.com>
2018-01-03 12:01:30 +0800

    update clients size mobile

--- pages/sections/clients.php
+++ pages/sections/clients.php
@@ -1,41 +1,41 @@
 <div class="container">
   <div class="row justify-content-center">
-    <div class="col-sm-6 col-md-4">
+    <div class="col-4 col-md-4x">
       <div class="img-wrapper text-center">
         <img src="/wp-content/uploads/2017/11/marriott.png" width="156px">
       </div>
     </div>
-    <div class="col-sm-6 col-md-4">
+    <div class="col-4 col-md-4x">
       <div class="img-wrapper text-center">
         <img src="/wp-content/uploads/2017/11/charlottes-w5.png" width="255px">
       </div>
     </div>
-    <div class="col-sm-6 col-md-4">
+    <div class="col-4 col-md-4x">
       <div class="img-wrapper text-center">
         <img src="/wp-content/uploads/2017/11/labs.png" width="187px">
       </div>
     </div>
-    <div class="col-sm-6 col-md-4 hidden-xs-down">
+    <div class="col-4 col-md-4x">
       <div class="img-wrapper text-center">
         <img src="/wp-content/uploads/2017/12/rut.png" width="121px">
       </div>
     </div>
-    <div class="col-sm-6 col-md-4 hidden-xs-down">
+    <div class="col-4 col-md-4x">
       <div class="img-wrapper text-center">
         <img src="/wp-content/uploads/2017/11/bnl.png" width="292px">
       </div>
     </div>
-    <div class="col-sm-6 col-md-4 hidden-xs-down">
+    <div class="col-4 col-md-4x">
       <div class="img-wrapper text-center">
         <img src="/wp-content/uploads/2017/11/sager-wild.png" width="195px">
       </div>
     </div>
-    <div class="col-sm-6 col-md-4 hidden-xs-down">
-      <div class="img-wrapper text-center mb0">
+    <div class="col-4 col-md-4x">
+      <div class="img-wrapper text-center">
         <img src="/wp-content/uploads/2017/11/goodman.png" width="358px">
       </div>
     </div>
-    <div class="col-sm-6 col-md-4 hidden-xs-down">
+    <div class="col-4 col-md-4x">
       <div class="img-wrapper text-center">
         <img src="/wp-content/uploads/2017/12/brewdog.png" width="87px">
       </div>
