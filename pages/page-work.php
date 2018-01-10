af14e993babd2f60cf9f582da3a05767396282d9
Romel Campos <rcoolah@gmail.com>
2018-01-10 18:17:01 +0800

    Tweak WWD

--- pages/page-work.php
+++ pages/page-work.php
@@ -1,43 +1,45 @@
-<?php
+ <div class="our-work-container">
+  <?php
 
-$args = array(
-  'category_name' => 'work',
-  'meta_key' => 'sort',
-  'orderby' => 'meta_value',
-  'order' => 'ASC'
-);
-$the_query = new WP_Query($args);
-while ($the_query->have_posts()) {
-  $the_query->the_post();
-  $background = get_post_meta($post->ID, 'background_image', true);
-  $right_title = get_post_meta($post->ID, 'right_title', true);
-  $right_content = get_post_meta($post->ID, 'right_content', true);
-  ?>
+  $args = array(
+    'category_name' => 'work',
+    'meta_key' => 'sort',
+    'orderby' => 'meta_value',
+    'order' => 'ASC'
+  );
+  $the_query = new WP_Query($args);
+  while ($the_query->have_posts()) {
+    $the_query->the_post();
+    $background = get_post_meta($post->ID, 'background_image', true);
+    $right_title = get_post_meta($post->ID, 'right_title', true);
+    $right_content = get_post_meta($post->ID, 'right_content', true);
+    ?>
 
-  <div class="work-list-section" style="background-image: url(<?php echo $background ?>);">
-    <div class="container text-white">
-      <div class="row justify-content-center">
-        <div class="col-lg-10">
-          <div class="row">
-            <div class="col-xs-10 col-md-10 col-lg-8">
-              <h1 class="text-uppercase"><?php echo the_title(); ?></h1>
-              <div class="text-bold">
-                <?php echo the_content(); ?>
+    <div class="work-list-section" style="background-image: url(<?php echo $background ?>);">
+      <div class="container text-white">
+        <div class="row justify-content-center">
+          <div class="col-lg-10">
+            <div class="row">
+              <div class="col-xs-10 col-md-10 col-lg-8">
+                <h1 class="text-uppercase"><?php echo the_title(); ?></h1>
+                <div class="text-bold">
+                  <?php echo the_content(); ?>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-4">
-              <div class="line-white-xs line-separator pull-left"></div>
-              <div class="work-right">
-                <p class="lead-xl text-bold"><?php echo $right_title; ?></p>
-                <?php echo $right_content; ?>
+              <div class="col-lg-4">
+                <div class="line-white-xs line-separator pull-left"></div>
+                <div class="work-right">
+                  <p class="lead-lg text-bold text-orange"><?php echo $right_title; ?></p>
+                  <?php echo $right_content; ?>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <?php
-}
-?>
+    <?php
+  }
+  ?>
+</div>
