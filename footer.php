5bad9a4aaa4fb2b0f6c87f8c3d24e9bed9cf10aa
Romel Campos <rcoolah@gmail.com>
2018-01-12 15:32:22 +0800

    Add GA

--- footer.php
+++ footer.php
@@ -25,6 +25,17 @@
         </div>
       </footer><!-- #colophon -->
     </div><!-- #page -->
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112236638-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-112236638-1');
+    </script>
+
     <?php wp_footer(); ?>
   </body>
 </html>
