e106aa00eeefa1e874313ee4459a25c05b648c0e
Romel Campos <rcoolah@gmail.com>
2018-02-06 16:32:52 +0800

    Editable logos

--- pages/sections/clients.php
+++ pages/sections/clients.php
@@ -1,45 +1,27 @@
 <div class="container">
   <p class="lead-lg text-bold text-center font-harmonia-black title">PEOPLE WE'VE WORKED WITH<span class="text-orange">.</span></p>
   <div class="row justify-content-center">
-    <div class="col-4 col-md-4x">
-      <div class="img-wrapper text-center">
-        <img src="/wp-content/uploads/2017/11/marriott.png" width="156px">
+    <?php
+    $args = array(
+      'category_name' => 'logo',
+      'meta_key' => 'sort',
+      'orderby' => 'meta_value',
+      'order' => 'ASC'
+    );
+
+    $the_query = new WP_Query($args);
+    while ($the_query->have_posts()) {
+      $the_query->the_post();
+      $logo = get_post_meta($post->ID, 'logo', true);
+      $width = get_post_meta($post->ID, 'width', true);
+      ?>
+
+      <div class="col-4">
+        <div class="img-wrapper text-center">
+          <img src="<?php echo $logo; ?>" width="<?php echo $width; ?>">
+        </div>
       </div>
-    </div>
-    <div class="col-4 col-md-4x">
-      <div class="img-wrapper text-center">
-        <img src="/wp-content/uploads/2017/11/charlottes-w5.png" width="255px">
-      </div>
-    </div>
-    <div class="col-4 col-md-4x">
-      <div class="img-wrapper text-center">
-        <img src="/wp-content/uploads/2017/11/labs.png" width="187px">
-      </div>
-    </div>
-    <div class="col-4 col-md-4x">
-      <div class="img-wrapper text-center">
-        <img src="/wp-content/uploads/2017/12/rut.png" width="121px">
-      </div>
-    </div>
-    <div class="col-4 col-md-4x">
-      <div class="img-wrapper text-center">
-        <img src="/wp-content/uploads/2017/11/bnl.png" width="292px">
-      </div>
-    </div>
-    <div class="col-4 col-md-4x">
-      <div class="img-wrapper text-center">
-        <img src="/wp-content/uploads/2017/11/sager-wild.png" width="195px">
-      </div>
-    </div>
-    <div class="col-4 col-md-4x">
-      <div class="img-wrapper text-center mb0">
-        <img src="/wp-content/uploads/2017/11/goodman.png" width="358px">
-      </div>
-    </div>
-    <div class="col-4 col-md-4x">
-      <div class="img-wrapper text-center mb0">
-        <img src="/wp-content/uploads/2017/12/brewdog.png" width="87px">
-      </div>
-    </div>
+
+    <?php } ?>
   </div>
 </div>
